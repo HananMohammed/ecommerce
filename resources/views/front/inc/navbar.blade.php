ad1eb1e5a8efd27064951955861bd1289a4b9a32
HananMohammed <hananmohammed2468@gmail.com>
2020-08-25 20:12:24 +0200

    Navbar responsive and media query

--- resources/views/front/inc/navbar.blade.php
+++ resources/views/front/inc/navbar.blade.php
@@ -1,7 +1,7 @@
 <div class="outer-nav">
     <!--Top Nav -->
-    <nav class=" top-nav navbar navbar-expand-lg navbar-light ">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <nav class=" top-nav navbar navbar-expand-md navbar-light ">
+        <button class="navbar-toggler" id="media-navbar" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
@@ -70,26 +70,93 @@
 
         </div>
     </nav>
-    <!--Seconf Nav-->
-    <div class="row">
-        <div class="col-md-2 ml-5">
+    <!--Second Nav-->
+    <div class="row m-auto">
+        <div class="col-md-2 ">
             <a class="navbar-brand" href="{{route('front.homepage')}}">
                 <img src="{{asset_public('images/[pro-demo.livelycart.com][190]logo.png')}}">
             </a>
         </div>
-        <div class="col-md-4">
+
+        <div class="col-lg-6 col-md-4  m-auto" >
             <form class="form-group">
-                <div class="input-group">
+                <div class="input-group  nav-search" >
                     <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
                     <div class="input-group-append">
-                        <span class="input-group-text" id="basic-addon1"><i class="fas fa-search"></i></span>
+                        <span class="input-group-text bg-light " id="basic-addon1"><i class="fas fa-search"></i></span>
                     </div>
                 </div>
             </form>
         </div>
-        <div class="col-md-4">
-        
+        <div class="col-md-2 col-sm-12 m-auto nav-login">
+            <div class="btn-group nav-login" role="group" aria-label="Basic example">
+                <button type="button" class="btn btn-light" style="line-height: 0!important;"><i class="fas fa-shopping-cart"></i> <label># item </label>/ <label>$ 0.00 USD </label></button>
+                <button type="button" class="btn btn-secondary">login</button>
+             </div>
         </div>
 
     </div>
 </div>
+
+
+<div class="wrapper">
+        <ul class="navbar-nav ">
+            <br>
+            <li class="nav-item active">
+                <a class="nav-link" href="{{route('front.homepage')}}">@lang('nav.home') <span class="sr-only">(current)</span></a>
+            </li>
+
+            <li class="nav-item dropdown ">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    @lang('nav.categories')
+                </a>
+
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <li><a class="dropdown-item" href="#">category 1</a></li>
+                    <li><a class="dropdown-item" href="#">category 2</a></li>
+                    <li><a class="dropdown-item" href="#">category 3</a></li>
+                    <li><a class="dropdown-item" href="#">category 1</a></li>
+                    <li><a class="dropdown-item" href="#">category 2</a></li>
+                    <li><a class="dropdown-item" href="#">category 3</a></li>
+                </ul>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    @lang('nav.brand')
+                </a>
+
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <li><a class="dropdown-item" href="#">brand 1</a></li>
+                    <li><a class="dropdown-item" href="#">brand 2</a></li>
+                    <li><a class="dropdown-item" href="#">brand 3</a></li>
+                    <li><a class="dropdown-item" href="#">brand 1</a></li>
+                    <li><a class="dropdown-item" href="#">brand 2</a></li>
+                    <li><a class="dropdown-item" href="#">brand 3</a></li>
+                    <li><a class="dropdown-item" href="#">brand 1</a></li>
+                    <li><a class="dropdown-item" href="#">brand 2</a></li>
+                    <li><a class="dropdown-item" href="#">brand 3</a></li>
+                    <li><a class="dropdown-item" href="#">brand 1</a></li>
+                    <li><a class="dropdown-item" href="#">brand 2</a></li>
+                    <li><a class="dropdown-item" href="#">brand 3</a></li>
+                </ul>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">@lang('nav.contact')</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><span class="glyphicon glyphicon-log-in"></span><span class="fas fa-sign-in-alt"></span>@lang('nav.login')</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fas fa-user-plus"></i>@lang('nav.register')</a>
+            </li>
+            <div class="dropdown-divider"></div>
+            <li class="nav-item" >
+                <a  class="nav-link" id="language" href="{{route('lang',trans('nav.langval'))}}"> @lang('nav.lang')</a>
+            </li>
+
+        </ul>
+
+    </div>
+
+
--- resources/views/front/layouts/master.blade.php
+++ resources/views/front/layouts/master.blade.php
@@ -5,11 +5,14 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title >Hanan Portfolio</title>
+    <title>Lively Cart Pro | @yield('title')</title>
+    <link rel="icon" href="{{asset_public('images/[pro-demo.livelycart.com][477]favicon.ico')}}" type="image/icon type">
     <link rel="stylesheet" type="text/css" href="{{URL::asset('public/css/style.css')}}" media="all"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
-     @if (session()->get('locale')=='ar'||session()->get('locale')==null)
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+    @if (session()->get('locale')=='ar'||session()->get('locale')==null)
         <link href="https://fonts.googleapis.com/css?family=Cairo:300,400,600,700&display=swap&subset=arabic" rel="stylesheet">
          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-rtl/3.4.0/css/bootstrap-rtl.min.css">
         <link rel="stylesheet" type="text/css" href="{{URL::asset('public/css/stylear.css')}}" media="all"/>
@@ -37,7 +40,26 @@
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
 <script src="{{ asset_public('js/index.js') }}"></script>
+<script>
+    $(document).ready(function () {
+        $('#media-navbar').on('click' ,function (event) {
+            event.preventDefault() ;
+            $('.wrapper').slideToggle(2000);
+            //
+            // console.log($('.wrapper').toggleClass('hidden'))
+            // if($('.wrapper').toggleClass('hidden') ){
+            //     $('#media-navbar').children().removeClass('fas fa-times fa-lg').addClass("navbar-toggler-icon")
+            // }else{
+            //     $('#media-navbar').children().removeClass('navbar-toggler-icon').addClass("fas fa-times fa-lg")
+            // }
+
+        });
+    });
+
+</script>
 </body>
 
 </html>
